07a4de0ff80955d46195cffce1fb2cea9e853a26
demie <demie@DESKTOP-G4R9TTK>
2023-05-29 18:59:34 +0300

    Add project files.

---
+++ Cryptocurrencie/ViewModels/ChangeTheme.cs
@@ -0,0 +1,26 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Windows;
+
+namespace Cryptocurrencie.ViewModels
+{
+	static class ChangeTheme
+	{
+		public static void ChangeThemeDark(bool darkMode=true)
+		{
+			PaletteHelper palette = new PaletteHelper();
+
+			ITheme theme = palette.GetTheme();
+
+			if (darkMode)
+			{
+				theme.SetBaseTheme(Theme.Dark);
+			}
+			else
+			{
+				theme.SetBaseTheme(Theme.Light);
+			}
+			palette.SetTheme(theme);
+		}
+	}
+}
